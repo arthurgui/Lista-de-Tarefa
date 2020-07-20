a7b8f2060aacdceefb72a6bc15003c4ecd394f35
arthur <arthurpereira.g@gmail.com>
2020-07-20 17:49:33 -0300

    Subindo projeto

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet...</p>
+        <h2>Desafio desenvolvedor java Full-Stack</h2>
+    </div>
+)
